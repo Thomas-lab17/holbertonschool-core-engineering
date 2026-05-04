7064650f2742cee8fe17aef7e2ade70fe1c34ea3
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-05-04 16:11:48 +0200

    Refactor last_digit.py to improve digit calculation and enhance print statement formatting

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
 number = __import__('random').randint(-10000, 10000)
-digit = abs(number) % 10
-if number < 0:
-    digit = -digit
-if digit > 5:
-    print(f"Last digit of {number} is {digit} and is greater than 5")
-    
-if digit == 0:
-    print(f"Last digit of {number} is {digit} and is 0")
 
-if digit < 6 and digit != 0:
-    print(f"Last digit of {number} is {digit} and is less than 6 and not 0")
+if number < 0:
+    digit = -(-number % 10)
+else:
+    digit = number % 10
+    
+if digit > 5:
+    print("Last digit of ",number ,"is", digit ," and is greater than 5")
+    
+elif digit == 0:
+    print("Last digit of ",number ,"is", digit ," and is 0")
+
+elif digit < 6 and digit != 0:
+    print("Last digit of",number ,"is", digit ,"and is less than 6 and not 0")
