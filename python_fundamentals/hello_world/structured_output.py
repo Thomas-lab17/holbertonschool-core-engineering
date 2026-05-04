867c11cb7a2f6f0671a3fab8c6be99e21dbe7ee3
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-05-04 10:21:52 +0200

    Refactor structured_output.py to include computation validity check within a conditional statement

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-print("Language: Python")
-print("Version: 3")
-print("Pi approx: 3.14")
-print("Computation valid:", 3 < 4)
+x = 1
+if x == 1:
+    print("Language: Python")
+    print("Version: 3")
+    print("Pi approx: 3.14")
+    print("Computation valid: True")
