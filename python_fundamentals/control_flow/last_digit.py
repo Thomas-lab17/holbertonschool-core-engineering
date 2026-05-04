25d33bd772d5e1cb62599414f2aefe98c64b46ab
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-05-04 14:37:11 +0200

    Add last_digit.py to determine the last digit of a random number

---
+++ python_fundamentals/control_flow/last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+number = __import__('random').randint(-10, 10)
+digit = number % 10
+if digit > 5:
+    print(f"Last digit of {number} is {digit} and is greater than 5")
+    
+if digit == 0:
+    print(f"Last digit of {number} is {digit} and is 0")
+
+if digit < 6 and digit != 0:
+    print(f"Last digit of {number} is {digit} and is less than 6 and not 0")
