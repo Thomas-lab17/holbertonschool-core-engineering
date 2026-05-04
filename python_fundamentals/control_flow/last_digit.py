b05e771d65b124135ddafd96d89ff6e280a594f2
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-05-04 15:50:00 +0200

    Refactor print statements in last_digit.py for consistency

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -8,10 +8,10 @@ if number > 0:
     digit = number % 10
     
 if digit > 5:
-    print(f"Last digit of {number} is {digit} and is greater than 5")
+    print("Last digit of ",number ,"is", digit ," and is greater than 5")
     
 if digit == 0:
-    print(f"Last digit of {number} is {digit} and is 0")
+    print("Last digit of ",number ,"is", digit ," and is 0")
 
 if digit < 6 and digit != 0:
-    print(f"Last digit of {number} is {digit} and is less than 6 and not 0")
+    print("Last digit of",number ,"is", digit ,"and is less than 6 and not 0")
