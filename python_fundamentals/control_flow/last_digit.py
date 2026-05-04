da33af6d2eb8d7710b22cfee543d70b5f68e845a
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-05-04 15:52:23 +0200

    Refactor last_digit.py to improve conditional structure for clarity

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -10,8 +10,8 @@ else:
 if digit > 5:
     print("Last digit of ",number ,"is", digit ," and is greater than 5")
     
-if digit == 0:
+elif digit == 0:
     print("Last digit of ",number ,"is", digit ," and is 0")
 
-if digit < 6 and digit != 0:
+elif digit < 6 and digit != 0:
     print("Last digit of",number ,"is", digit ,"and is less than 6 and not 0")
