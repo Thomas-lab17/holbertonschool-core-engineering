ec6bff77d7f3a4c66ea3e409747e18ae6504758b
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-05-05 09:55:21 +0200

    Refactor print_alphabt.py to simplify letter printing using string replacement

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,8 +1,3 @@
 #!/usr/bin/env python3
 
-i = 97
-while i < 123:
-    if chr(i) not in "eq":
-        print(chr(i), end='')
-    i += 1
-print()
+print("abcdefghijklmnopqrstuvwxyz".replace("e", "").replace("q", ""))
