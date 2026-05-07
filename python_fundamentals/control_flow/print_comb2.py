8c6760b1ecb60a6837672c5ad766add8738a8c52
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-05-07 12:20:13 +0200

    Fix: update print_comb2.py and print_comb3.py for correct number formatting

--- python_fundamentals/control_flow/print_comb2.py
+++ python_fundamentals/control_flow/print_comb2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
-for i in range(99):
-    print("{:02d}".format(i), end=", ")
-print("{:02d}".format(99))
+for i in range(100):
+    if i != 99:
+        print("{:02d}".format(i), end=", ")
+    else:
+        print("{:02d}".format(i))
