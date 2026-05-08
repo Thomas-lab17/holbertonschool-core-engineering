e301d946344f928f578485f6fb9cc34a47fa7c33
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-05-08 19:11:42 +0200

    Remove docstring from print_list_integer function

--- python_fundamentals/core_data_structures/print_list_integer.py
+++ python_fundamentals/core_data_structures/print_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 def print_list_integer(my_list=[]):
-    """Print all integers of a list, one per line."""
 
     for value in my_list:
         print("{:d}".format(value))
