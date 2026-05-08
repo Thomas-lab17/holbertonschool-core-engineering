f54523b0d89330d0b5c84bb7bf3462b4da844b54
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-05-08 18:55:16 +0200

    Remove docstring from pow function for clarity

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 def pow(a, b):
-    """Return a raised to the power of b using a loop.
-    Assumes b is a non‑negative integer.
-    """
     result = 1
     for _ in range(b):
         result *= a
