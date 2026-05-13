ca2c8e2807d64a4fd8a64636f2fef46d89a40d8e
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-05-13 14:49:54 +0200

    Add: enhance Square class docstring for clarity

--- python_oop/classes_and_object_model/0-square.py
+++ python_oop/classes_and_object_model/0-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
-class Square:
-    pass
+"""
+Module that defines an empty Square class.
+"""
 
+
+class Square:
+    """
+    A class that defines a square.
+
+    This is an empty class for the first task.
+    """
+    pass
