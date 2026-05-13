64972ea50cc9499ce20718dd9430a74e8e1ae863
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-05-13 14:54:57 +0200

    Add: enhance docstrings in Square class for better clarity

--- python_oop/classes_and_object_model/1-square.py
+++ python_oop/classes_and_object_model/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+"""Defines a Square class with a private size attribute."""
 
 class Square:
+    """Represents a square with encapsulated size information."""
 
     def __init__(self, size):
+        """Initializes a new Square instance with a private size attribute."""
         self.__size = size
