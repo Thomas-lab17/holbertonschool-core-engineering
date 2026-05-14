63ce4a576f145153c8fa9c2a1333cfd743ac2ab5
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-05-14 14:38:45 +0200

    Fix: set default size parameter in Square class constructor

--- python_oop/classes_and_object_model/2-square.py
+++ python_oop/classes_and_object_model/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Class that defines a square with a private size attribute."""
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initialize a Square instance.
 
         Args:
@@ -15,7 +15,7 @@ class Square:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         """
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
