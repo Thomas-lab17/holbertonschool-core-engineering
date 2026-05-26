d7d92ab8ea286a208c89a0694b406b80bf0ed424
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-05-26 10:21:38 +0200

    Add: implement append_write function to append text to a file

---
+++ python_advanced/file_handling/append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Defines an append_write function."""
+
+def append_write(filename="", text=""):
+    """Appends a string to a text file (UTF8) and returns the
+    number of characters added
+    Args:
+        filename (str): The name of the file to append to
+        text (str): The string to append to the file
+    Returns:
+        int: The number of characters added
+    """
+    with open(filename, 'a', encoding='utf-8') as MyFile:
+        return MyFile.write(text)
