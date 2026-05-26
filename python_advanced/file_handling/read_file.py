3c6a1edcd1cd4c62482ee6384ce9bb722a390e2e
Thomas-lab17 <vbizzle.minecraft@gmail.com>
2026-05-26 10:20:40 +0200

    Fix: standardize variable name to MyFile in read_file and write_file functions for consistency

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -8,5 +8,5 @@ def read_file(filename=""):
     Args:
         filename (str): The name of the file to read.
     """
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    with open(filename, encoding="utf-8") as MyFile:
+        print(MyFile.read(), end="")
